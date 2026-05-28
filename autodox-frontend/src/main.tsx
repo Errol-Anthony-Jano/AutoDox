395dd7a40e3c5f309bd625a6c68d797b09dc8bc5
Errol Anthony Jano <200910884@my.xu.edu.ph>
2026-05-29 00:21:40 +0800

    Setup routes for the dashboard

--- autodox-frontend/src/main.tsx
+++ autodox-frontend/src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Overview from './pages/Dashboard/Overview/Overview.tsx'
+import MemberSubmittedProposals from './pages/Dashboard/SubmittedProposals/MemberSubmittedProposals.tsx'
+import Dashboard from './pages/Dashboard/Dashboard.tsx'
+import Members from './pages/Dashboard/Members/Members.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />}>
+          <Route index element={<Overview />} />
+          <Route path="/submitted-proposals" element={<MemberSubmittedProposals />} />
+          <Route path="/members" element={<Members />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
